2f915f9a22a443eae52204fb3f057d111b311732
BereczkiNorbertCristian <bnorbertcristian@gmail.com>
2018-04-14 17:34:04 +0300

    Added dataset

---
+++ experimets/experiment 2/extract_dataset.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo "usage: $0 <#extraciton_number>"
+	exit 1;
+else
+	echo "Beginning extraction..."
+fi
+
+DATA_EXTRACTION_NO=$1
+EXTRACTION_FOLDER="dataset_$DATA_EXTRACTION_NO"
+mkdir $EXTRACTION_FOLDER
+
+for dir in `ls dataset`; do
+	DIR="dataset/$dir"
+	for file in `ls $DIR`; do
+		cp $DIR/$file $EXTRACTION_FOLDER/${dir}_${file}
+	done;
+done;
+
+for file in $EXTRACTION_FOLDER/*.csv; do
+	sed -i "s/\//\_/g" $file
+done;
+
+BBPATH=$EXTRACTION_FOLDER/bboxes.csv
+touch $BBPATH
+
+for file in $EXTRACTION_FOLDER/*.csv; do
+	tail -n +2 $file >> $BBPATH
+done;
+
+echo "Extraction ended..."
+echo "Image stats"
+file $EXTRACTION_FOLDER/*.jpg | cut -d "," -f8 | uniq -c
+
